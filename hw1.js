6338fe589ada7ce6dce84fc89020ffcafaef35fc
Dpinchuk20 <60977239+Dpinchuk20@users.noreply.github.com>
2020-02-26 14:10:55 -0500

    Update hw1.js

--- hw1.js
+++ hw1.js
@@ -6,59 +6,33 @@
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width">
+<head>
+<style>
+.cities {
+  background-color: black;
+  color: white;
+  margin: 20px;
+  padding: 20px;
+}
+</style>
+</head>
+<body>
 
-    <title>Prices for Groceries</title>
-
-    <link rel="stylesheet" href="">
-    <!--[if lt IE 9]>
-      <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>           
-    <![endif]-->
-
-  </head>
-
-  <body>
-      <h1>Prices </h1>
-
-
-<div class="form">
-  <label for="item">Add item to list: </label><input type="text" id="item">
-  <button id="submit">Submit item</button>
-  <button id="clear">Clear items</button>
+<div class="name">
+  <h2>Apples</h2>
+  <p>Apples will cost two dollars.</p>
+  <p1>There are a grant total of <int>5</int>Apples</p1>
 </div>
 
-<ul id="list">
+<div class="price">
+  <h2>Milk</h2>
+  <p>Milk will cost five dollars.</p>
+</div>
 
-</ul>
+<div class="count">
+  <h2>Tokyo</h2>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
 
-
-  </body>
-
-  <script>
-   var itemInput = document.getElementById("item");
-   var clear = document.getElementById("clear");
-   var submit = document.getElementById("submit");
-   var list = document.getElementById("list");
-
-   function addItem() {
-     var currentItem = itemInput.value;
-     var listItem = document.createElement("li");
-     listItem.innerHTML = currentItem;
-
-     list.appendChild(listItem);
-
-     itemInput.value = "";
-   }
-
-   submit.onclick = addItem;
-
-   clear.onclick = function() {
-     list.innerHTML = "";
-     itemInput.value = "";
-   }
-
-  </script>
+</body>
 </html>
